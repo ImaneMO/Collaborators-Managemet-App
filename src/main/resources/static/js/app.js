c6568d2afaa062d4bb8dfdf7ddf6ad435014bf88
unknown <imaneanbar7@gmail.com>
2020-03-04 18:58:29 +0100

    Front completed

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,118 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  
+  $('.pre-footer').hover(function(){
+    showPreFooter();
+  },function(){
+    hidePreFooter();
+  })
+  
+});
 
-  function ajaxCallRequest(f_method, f_url, f_data) {
-    $("#dataSent").val(unescape(f_data));
-    var f_contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
-    $.ajax({
-      url: f_url,
-      type: f_method,
-      contentType: f_contentType,
-      dataType: 'json',
-      data: f_data,
-      success: function(data) {
-        var jsonResult = JSON.stringify(data);
-        $("#results").val(unescape(jsonResult));
-      }
-    });
-  }
+function showPreFooter(){
+  var preFooter=$('.pre-footer');
+  var preFooterHeight=preFooter.innerHeight();
 
-
-  var formResults = JSON.stringify({
-   first_name = document.querySelector("#first_name"),
-   last_name = document.querySelector("#last_name"),
-   civility = document.querySelector("#civility"),
-   email = document.querySelector("#email"),
-   phone_number = document.querySelector("#phone_number")
-  });
-
-
-$("#sendSerialized").click(function(event) {
-    event.preventDefault();
-    var form = $('#contact_form');
-    var method = form.attr('method');
-    var url = form.attr('action')/* + 'serialized/'*/;
-    var objectData = $(form).serializeObject();
-    var data = JSON.stringify(objectData);
-    
-    console.log(data);
-    ajaxCallRequest(method, url, data);
-  });
-  $.mockjax({
-    url: 'http://localhost:8080/api/collaborators/',
-    type: 'POST',
-    contentType: 'application/x-www-form-urlencoded',
-    responseTime: 0,
-    response: function(settings) {
-      var data = settings.data;
-      this.responseText = data;
-    }
-  });
-
-
-   //form serializer
-(function($) {
-  $.fn.serializeObject = function() {
-
-    var self = this,
-      json = {},
-      push_counters = {},
-      patterns = {
-        "validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
-        "key": /[a-zA-Z0-9_]+|(?=\[\])/g,
-        "push": /^$/,
-        "fixed": /^\d+$/,
-        "named": /^[a-zA-Z0-9_]+$/
-      };
-      
-    this.pair = function(base, key, value) {
-      base[key] = value;
-      return base;
-    };
-
-    this.push_counter = function(key) {
-      if (push_counters[key] === undefined) {
-        push_counters[key] = 0;
-      }
-      return push_counters[key]++;
-    };
-
-    $.each($(this).serializeArray(), function(index, item) {
-
-      // skip invalid keys
-      var name = item.name;
-      if (!patterns.validate.test(name)) {
-        return;
-      }
-
-      var key;
-      var keys = name.match(patterns.key);
-      var value = item.value;
-      var reverse_key = name;
-
-      while ((key = keys.pop()) !== undefined) {
-
-        // adjust reverse_key
-        reverse_key = reverse_key.replace(new RegExp("\\[" + key + "\\]$"), '');
-
-        // push
-        if (key.match(patterns.push)) {
-          value = self.pair([], self.push_counter(reverse_key), value);
-        }
-
-        // fixed
-        else if (key.match(patterns.fixed)) {
-          value = self.pair([], key, value);
-        }
-
-        // named
-        else if (key.match(patterns.named)) {
-          value = self.pair({}, key, value);
-        }
-      }
-
-      json = $.extend(true, json, value);
-    });
-
-    return json;
-  };
-})(jQuery);
+  preFooter.stop().animate({top:'-'+preFooterHeight+'px'},500,'swing');
+}
+function hidePreFooter(){
+  var preFooter=$('.pre-footer');
+  preFooter.stop().animate({top:'-40px'},500,'swing');
+}
